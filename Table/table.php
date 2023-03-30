59e7c50361b97199ffce821bca545eaef9f8ce29
Mega Lanalee Arbolado <megelanaleearbolado@gmail.com>
2023-03-30 11:33:40 +0800

    change

--- Table/table.php
+++ Table/table.php
@@ -6,33 +6,42 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>About Myself</title>
+
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+   integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+   <link rel="stylesheet" href="../style.css">
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+   integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
    <link rel="stylesheet" href="css/font-awesome.min.css">
    <link rel="stylesheet" href="css/bootstrap.min.css">
    <link rel="stylesheet" href="mysql/localhost/contact_db/contact">
    <link rel="stylesheet" href="index.php">
+   <link rel="stylesheet" href="delete.php">
 
 </head>
 
 <body>
-   <div class="container my-5">
-      <h2>Table</h2>
+   <div class="container text-center my-5">
+      <div class="text-custom fw-bold fs-2 mt-2 mb-4">TABLE</div>
      
-      <br>
-      <table class="table">
-         <thead>
-            <tr>
-               <th scope = "col">ID</th>
-               <th scope = "col">Email</th>
-               <th scope = "col">Name</th>
-               <th scope = "col">Message</th>
-            </tr>
-         </thead>
-         <tbody>
-            <?php
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $database = "contact_db";
+     <br>
+     <table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">EMAIL</th>
+      <th scope="col">NAME</th>
+      <th scope="col">MESSAGE</th>
+      <th scope="col">ACTION</th>
+    </tr>
+  </thead>
+        <tbody>
+           <?php
+           $servername = "localhost";
+           $username = "root";
+           $password = "";
+           $database = "contact_db";   
 
             // Create connection
             $connection = new mysqli($servername, $username, $password, $database);
@@ -59,17 +68,23 @@
                   <td><?= $value['2'] ?></td>
                   <td><?= $value['3'] ?></td>
 
-                  <td>
-                     <a class='btn btn-primary btn-sm' href=' /contactt_db/edit.php?id=$row[id]'>Edit</a>
-                     <a class='btn btn-primary btn-sm' href='/contact_db/delete.php?id=$row[id]'>Delete</a>
-                  </td>
+                 
+                     <td>
+                                <form action="delete.php" method="post">
+                                    <input type="hidden" name="id" value="<?php echo $rows['id']?>">
+                                    <input type="submit" name="delete" class="btn btn-outline-danger btn-sm" value="DELETE">
+                                </form>
+                            </td>
+                  
                </tr>
             <?php endforeach ?>
          </tbody>
       </table>
    </div>
-   <div class="bg-custom text-center py-5">
+   <div class="container">
+   <div class="bg-custom text-center py-4">
         <br> <br><br><button class="btn btn-outline-danger " onclick="history.back()">Home</button>
+      </div>
       </div>
    <script src="js/bootstrap.bundle.min.js"></script>
 
